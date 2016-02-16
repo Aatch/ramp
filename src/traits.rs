d170d70d25dae257ffefee261ce8e82be7c1090f
Aaron Gallagher <_@habnab.it>
2016-02-16 01:59:46 -0800

    Add the DivRem trait and implement it for Int.
    This means that divrem can be done more efficiently for (Int, usize)
    -> (Int, usize) than having to round-trip through an Int.
    
    Probably a few more impls of DivRem are required, but this is a base to
    work from at least.

--- src/traits.rs
+++ src/traits.rs
@@ -12,27 +12,9 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#![crate_type="lib"]
-#![crate_name="ramp"]
+pub trait DivRem<RHS = Self> {
+    // Default to (Self, RHS) when associated type defaults are more stable.
+    type Output;
 
-#![feature(core_intrinsics, asm, heap_api, associated_consts)]
-#![feature(zero_one, step_trait, unique, alloc)]
-
-#![cfg_attr(test, feature(test))]
-
-#[cfg(test)] extern crate test;
-
-extern crate alloc;
-extern crate rand;
-extern crate hamming;
-
-pub mod ll;
-mod mem;
-
-pub mod traits;
-pub mod int;
-
-// Re-exports
-
-pub use int::Int;
-pub use int::RandomInt;
+    fn divrem(self, rhs: RHS) -> Self::Output;
+}
